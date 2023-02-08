a6aca647898014d5438f821a9db4b71e2e39252f
Evaneta <eftimovaevaneta@gmail.com>
2023-02-08 22:08:37 +0200

    Update Tags.js

--- src/Tags.js
+++ src/Tags.js
@@ -2,7 +2,7 @@ export default function Tags(props) {
     return (
         <div className="tags">
             {props.tags.map(tag =>{
-                return <tag className="tag" tags={tag}></tag>
+                return <tag className="tag" tags={tag}>#{tag}</tag>
             })}
         </div>
     )
